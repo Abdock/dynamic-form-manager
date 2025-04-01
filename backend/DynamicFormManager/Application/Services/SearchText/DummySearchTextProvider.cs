8be1caba1e3be6ada843221bf745b8cab390212e
Abdussattar Kassymbekov <abdusattar.kasymbekov@gmail.com>
2025-04-01 21:36:21 +0500

    added default search text provider

---
+++ backend/DynamicFormManager/Application/Services/SearchText/DummySearchTextProvider.cs
@@ -0,0 +1,36 @@
+﻿using System.Buffers.Text;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace Application.Services.SearchText;
+
+public class DummySearchTextProvider : ISearchTextProvider
+{
+    public string GetSearchText(JsonNode node)
+    {
+        var searchTextBuilder = new StringBuilder();
+        foreach (var (_, value) in node.AsObject())
+        {
+            if (value is null)
+            {
+                continue;
+            }
+
+            if (value.GetValueKind() != JsonValueKind.String)
+            {
+                continue;
+            }
+
+            var propertyValue = value.GetValue<string>();
+            if (Base64.IsValid(propertyValue))
+            {
+                continue;
+            }
+
+            searchTextBuilder.AppendLine(propertyValue);
+        }
+
+        return searchTextBuilder.ToString();
+    }
+}
